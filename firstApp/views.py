c4628c67b00ce27c193725755bc9e881e0ad2994
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-12 16:33:26 +0530

    quote app & code for first app added

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
 
 # Create your views here.
+def display(request):
+    return HttpResponse("<h1>My First Django App!</h1>")
+
+
+def displayDateTime(request):
+    current_datetime = datetime.datetime.now()
+    current_datetime_str = "Current Date and Time is " + str(current_datetime)
+    return HttpResponse(current_datetime_str)
